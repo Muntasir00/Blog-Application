a395cf42040715ebb459b0c820ddacafb372d62b
Muntasir00 <muntasir690000.com>
2023-12-13 16:07:21 +0600

    User functionality completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,45 +1,47 @@
 class ArticlesController < ApplicationController
-    before_action :set_article, only: [:show, :edit, :update, :destroy]
-    def show
-        
-    end
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  def show
+      
+  end
 
-    def index
-        @articles = Article.all
-    end
+  def index
+      @articles = Article.paginate(page: params[:page], per_page: 5)
+  end
 
-    def new
-        @article = Article.new
-    end
+  def new
+      @article = Article.new
+  end
 
-    def edit
-        
-    end
+  def edit
+      
+  end
 
-    def create 
-      @article = Article.new(articles_params) 
-      @article.user = User.last
-      if @article.save 
-        flash[:notice] = "Article was created successfully." 
-        redirect_to @article 
-      else 
-        render 'new', status: :unprocessable_entity
-      end 
-    end
+  def create 
+    @article = Article.new(articles_params) 
+    @article.user = User.last
+    if @article.save 
+      flash[:notice] = "Article was created successfully." 
+      redirect_to @article 
+    else 
+      render 'new', status: :unprocessable_entity
+    end 
+  end
 
-    def update
-        if @article.update(articles_params)
-            flash[:notice] = "Article was updated successfully."
-            redirect_to @article 
-        else
-            render 'edit', status: :unprocessable_entity
-        end
-    end
+  def update
+      if @article.update(articles_params)
+          flash[:notice] = "Article was updated successfully."
+          redirect_to @article 
+      else
+          render 'edit', status: :unprocessable_entity
+      end
+  end
 
-    def destroy 
-        @article.destroy                     
-        redirect_to articles_path    
-    end
+  def destroy 
+      @article.destroy                     
+      redirect_to articles_path    
+  end
     
   private
 
@@ -51,4 +53,13 @@ class ArticlesController < ApplicationController
     params.require(:article).permit(:title, :description)
   end
 
+  def require_same_user
+    if current_user != @article.user
+      flash[:alert] = "You can only edit or delete your own article"
+      redirect_to @article
+    end 
+  end
+
+
 end
+
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
     belongs_to :user
     validates :title, presence: true, length: {minimum: 6, maximum: 100}
-    validates :description, presence: true, length: {minimum: 6, maximum: 100}
+    validates :description, presence: true, length: {minimum: 6, maximum: 100000}
     belongs_to :user
 end
